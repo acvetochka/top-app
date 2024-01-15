29ad64168bff5204fe07c67fb381487a32b03b78
Alona Kuznietsova <acvetochka@gmail.com>
2024-01-15 22:51:37 +0100

    add products api

---
+++ api/product.tsx
@@ -0,0 +1,20 @@
+import { API } from "@/app/api";
+import { ProductModel } from "@/interfaces/product.interface";
+
+export async function getProduct(category: string): Promise<ProductModel[] | null> {
+  const res = await fetch(API.product.find, {
+    method: "POST",
+    body: JSON.stringify({
+      category,
+      limit: 10,
+    }),
+    headers: new Headers({ "content-type": "application/json" }),
+    next: { revalidate: 10 },
+  });
+
+  if (!res.ok) {
+    return null;
+  }
+
+  return res.json();
+}
