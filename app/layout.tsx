069fbce3cc9b550f33c7a04802f07396398bc130
Alona Kuznietsova <acvetochka@gmail.com>
2024-01-16 23:28:10 +0100

    update Description

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={notoSans.className}>
-        <Header />
+        {/* <Header /> */}
         <Sidebar />
         {children}
         <Footer />
--- sections/Description/Description.tsx
+++ sections/Description/Description.tsx
@@ -1,12 +1,22 @@
 import { Tag, Title } from "@/components";
 import { DescriptionProps } from "./Description.props";
+import styles from "./Description.module.css";
+import { HhData } from "../HhData/HhData";
 
-export const Description = ({ page, products, firstCategory }: DescriptionProps): JSX.Element => {
+export const Description = ({ page, products }: DescriptionProps): JSX.Element => {
   return (
-    <div className="">
-      <Title tag="h1">{page.title}</Title>
-      {products && <Tag color="gray">{products.length}</Tag>}
-      <span>Сортировка</span>
+    <div className={styles.wrapper}>
+      <div className={styles.title}>
+        <Title tag="h1">{page.title}</Title>
+        {products && <Tag color="gray">{products.length}</Tag>}
+        <span>Сортировка</span>
+      </div>
+      <div>{products && products.map((p) => <div key={p._id}>{p.title}</div>)}</div>
+      <div className={styles.hhTitle}>
+        <Title tag="h2">Вакансии - {page.category}</Title>
+        <Tag color="red">hh.ru</Tag>
+      </div>
+      {page.hh && <HhData {...page.hh} />}
     </div>
   );
 };
