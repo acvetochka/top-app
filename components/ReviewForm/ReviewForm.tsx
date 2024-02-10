b2c0a498140352da565a80d7249dbd9d09beb781
Alona Kuznietsova <acvetochka@gmail.com>
2024-02-10 23:14:56 +0100

    change rating

--- components/ReviewForm/ReviewForm.tsx
+++ components/ReviewForm/ReviewForm.tsx
@@ -1,21 +1,20 @@
-"use client";
+'use client';
 
-import styles from "./ReviewForm.module.css";
-import cn from "classnames";
-import { ReviewFormProps } from "./ReviewForm.props";
-import { Input } from "../Input/Input";
-import { Rating } from "../Rating/Rating";
-import { Textarea } from "../Textarea/Textarea";
-import { Button } from "../Button/Button";
-import CloseIcon from "@/public/closeReview.svg";
-import { useForm, Controller } from "react-hook-form";
-import { IReviewForm, IReviewSendResponse } from "./ReviewForm.interface";
-import axios from "axios";
-import { API } from "@/app/api";
-import { useState } from "react";
-import { ErrorProps } from "next/error";
+import styles from './ReviewForm.module.css';
+import cn from 'classnames';
+import { ReviewFormProps } from './ReviewForm.props';
+import { Input } from '../Input/Input';
+import { Rating } from '../Rating/Rating';
+import { Textarea } from '../Textarea/Textarea';
+import { Button } from '../Button/Button';
+import CloseIcon from '@/public/closeReview.svg';
+import { useForm, Controller } from 'react-hook-form';
+import { IReviewForm, IReviewSendResponse } from './ReviewForm.interface';
+import axios from 'axios';
+import { API } from '@/app/api';
+import { useState } from 'react';
 
-export const ReviewForm = ({ productId, className, ...props }: ReviewFormProps) => {
+export const ReviewForm = ({ productId, isOpened, className, ...props }: ReviewFormProps) => {
   const {
     register,
     control,
@@ -28,32 +27,70 @@ export const ReviewForm = ({ productId, className, ...props }: ReviewFormProps) 
 
   const onSubmit = async (formData: IReviewForm) => {
     try {
-      const { data } = await axios.post<IReviewSendResponse>(API.review.createDemo, { ...formData, productId });
+      const { data } = await axios.post<IReviewSendResponse>(API.review.createDemo, {
+        ...formData,
+        productId,
+      });
       console.log(data);
       if (data.message) {
         setIsSuccess(true);
         reset();
       } else {
-        setError("Что-то пошло не так");
+        setError('Что-то пошло не так');
       }
-    } catch (error) {
-      setError(error.message);
+    } catch (e) {
+      setError(e.message);
     }
   };
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
       <div className={cn(styles.reviewForm, className)} {...props}>
-        <Input {...register("name", { required: { value: true, message: "Заполните имя" } })} placeholder="Имя" error={errors.name} />
-        <Input {...register("title", { required: { value: true, message: "Заполните заголовок" } })} placeholder="Заголовок отзыва" className={styles.title} error={errors.title} />
+        <Input
+          {...register('name', { required: { value: true, message: 'Заполните имя' } })}
+          placeholder="Имя"
+          error={errors.name}
+          tabIndex={isOpened ? 0 : -1}
+        />
+        <Input
+          {...register('title', { required: { value: true, message: 'Заполните заголовок' } })}
+          placeholder="Заголовок отзыва"
+          className={styles.title}
+          error={errors.title}
+          tabIndex={isOpened ? 0 : -1}
+        />
         <div className={styles.rating}>
           <span>Оценка</span>
-          <Controller rules={{ required: { value: true, message: "Укажите оценку" } }} control={control} name="rating" render={({ field }) => <Rating isEditable rating={field.value} ref={field.ref} setRating={field.onChange} error={errors.rating} />} />
+          <Controller
+            rules={{ required: { value: true, message: 'Укажите оценку' } }}
+            control={control}
+            name="rating"
+            render={({ field }) => (
+              <Rating
+                isEditable
+                rating={field.value}
+                ref={field.ref}
+                setRating={field.onChange}
+                error={errors.rating}
+                tabIndex={isOpened ? 0 : -1}
+              />
+            )}
+          />
         </div>
-        <Textarea {...register("description", { required: { value: true, message: "Заполните описание" } })} placeholder="Текст отзыва" className={styles.description} error={errors.description} />
+        <Textarea
+          {...register('description', { required: { value: true, message: 'Заполните описание' } })}
+          placeholder="Текст отзыва"
+          className={styles.description}
+          error={errors.description}
+          tabIndex={isOpened ? 0 : -1}
+        />
         <div className={styles.submit}>
-          <Button appearance="primary">Отправить</Button>
-          <span className={styles.info}>* Перед публикацией отзыв пройдет предварительную модерацию и проверку</span>
+          <Button appearance="primary" tabIndex={isOpened ? 0 : -1}>
+            Отправить
+          </Button>
+          <span className={styles.info}>
+            * Перед публикацией отзыв пройдет предварительную модерацию и проверку
+          </span>
         </div>
       </div>
       <div>
--- sections/Product/Product.tsx
+++ sections/Product/Product.tsx
@@ -1,89 +1,138 @@
-"use client";
+'use client';
 
-import cn from "classnames";
-import Image from "next/image";
+import cn from 'classnames';
+import Image from 'next/image';
 
-import { ProductProps } from "./Product.props";
-import { Button, Card, Divider, ProductAdvantages, ProductFeatures, ProductPrice, Rating, Review, ReviewForm, Tag } from "@/components";
-import { devOfNum } from "@/helpers/devOfNum";
-import styles from "./Product.module.css";
-import { ForwardedRef, forwardRef, useEffect, useRef, useState } from "react";
-import { motion } from "framer-motion";
+import { ProductProps } from './Product.props';
+import {
+  Button,
+  Card,
+  Divider,
+  ProductAdvantages,
+  ProductFeatures,
+  ProductPrice,
+  Rating,
+  Review,
+  ReviewForm,
+  Tag,
+} from '@/components';
+import { devOfNum } from '@/helpers/devOfNum';
+import styles from './Product.module.css';
+import { ForwardedRef, forwardRef, useEffect, useRef, useState, KeyboardEvent } from 'react';
+import { motion } from 'framer-motion';
 
 export const Product = motion(
-  forwardRef(({ product, className, ...props }: ProductProps, ref: ForwardedRef<HTMLDivElement>): JSX.Element => {
-    const [isReviewOpened, setIsReviewOpened] = useState<boolean>(false);
+  forwardRef(
+    (
+      { product, className, ...props }: ProductProps,
+      ref: ForwardedRef<HTMLDivElement>
+    ): JSX.Element => {
+      const [isReviewOpened, setIsReviewOpened] = useState<boolean>(false);
 
-    const variants = {
-      visible: { opacity: 1, height: "auto", visibility: "visible", pointerEvents: "auto" },
-      hidden: { opacity: 0, height: "0", visibility: "hidden", pointerEvents: "none" },
-    };
-    const reviewRef = useRef<HTMLDivElement>(null);
+      const variants = {
+        visible: { opacity: 1, height: 'auto' },
+        hidden: { opacity: 0, height: '0' },
+      };
+      const reviewRef = useRef<HTMLDivElement>(null);
 
-    const scrollToReview = () => {
-      setIsReviewOpened(true);
-      setTimeout(() => {
-        reviewRef.current?.scrollIntoView({
-          behavior: "smooth",
-          block: "start",
-        });
-      }, 1000);
-      reviewRef.current?.focus();
-    };
+      const scrollToReview = () => {
+        setIsReviewOpened(true);
+        setTimeout(() => {
+          reviewRef.current?.scrollIntoView({
+            behavior: 'smooth',
+            block: 'start',
+          });
+        }, 300);
+        reviewRef.current?.focus();
+      };
 
-    useEffect(() => {
-      setIsReviewOpened(isReviewOpened);
-    }, [isReviewOpened]);
+      const handleKeyDown = (e: KeyboardEvent) => {
+        if (e.code === 'Space' || e.code === 'Enter') {
+          e.preventDefault();
+          scrollToReview();
+        }
+      };
 
-    return (
-      <div className={className} {...props} ref={ref}>
-        <Card className={styles.product}>
-          <div className={styles.logo}>
-            <Image src={process.env.NEXT_PUBLIC_DOMAIN + product.image} alt={product.title} width={70} height={70} />
-          </div>
-          <div className={styles.title}>{product.title}</div>
-          <ProductPrice price={product.price} oldPrice={product.oldPrice} credit={product.credit} />
-          <div className={styles.rating}>
-            <Rating rating={product.reviewAvg ?? product.initialRating} />
-          </div>
-          <div className={styles.tags}>
-            {product.categories.map((c) => (
-              <Tag className={styles.category} key={c} color="ghost">
-                {c}
-              </Tag>
-            ))}
-          </div>
-          <div className={styles.priceTitle}>цена</div>
-          <div className={styles.creditTitle}>кредит</div>
-          <div className={styles.rateTitle}>
-            <a href="#ref" onClick={scrollToReview}>
-              {product.reviewCount} {devOfNum(product.reviewCount, ["отзыв", "отзыва", "отзывов"])}
-            </a>
-          </div>
-          <Divider className={styles.hr} />
-          <div className={styles.description}>{product.description}</div>
-          <ProductFeatures characteristics={product.characteristics} />
-          <ProductAdvantages advantages={product.advantages} disadvantages={product.disadvantages} />
-          <Divider className={cn(styles.hr, styles.hr2)} />
-          <div className={styles.actions}>
-            <Button appearance="primary">Узнать подробнее</Button>
-            <Button appearance="ghost" arrow={isReviewOpened ? "down" : "right"} className={styles.reviewButton} onClick={() => setIsReviewOpened(!isReviewOpened)}>
-              Читать отзывы
-            </Button>
-          </div>
-        </Card>
-        <motion.div animate={isReviewOpened ? "visible" : "hidden"} variants={variants} initial="hidden">
-          <Card color="blue" className={cn(styles.reviews)} ref={reviewRef}>
-            {product.reviews.map((r) => (
-              <div key={r._id}>
-                <Review review={r} />
-                <Divider />
-              </div>
-            ))}
-            <ReviewForm productId={product._id} />
+      useEffect(() => {
+        setIsReviewOpened(isReviewOpened);
+      }, [isReviewOpened]);
+
+      return (
+        <div className={className} {...props} ref={ref}>
+          <Card className={styles.product}>
+            <div className={styles.logo}>
+              <Image
+                src={process.env.NEXT_PUBLIC_DOMAIN + product.image}
+                alt={product.title}
+                width={70}
+                height={70}
+              />
+            </div>
+            <div className={styles.title}>{product.title}</div>
+            <ProductPrice
+              price={product.price}
+              oldPrice={product.oldPrice}
+              credit={product.credit}
+            />
+            <div className={styles.rating}>
+              <Rating rating={product.reviewAvg ?? product.initialRating} />
+            </div>
+            <div className={styles.tags}>
+              {product.categories.map(c => (
+                <Tag className={styles.category} key={c} color="ghost">
+                  {c}
+                </Tag>
+              ))}
+            </div>
+            <div className={styles.priceTitle}>цена</div>
+            <div className={styles.creditTitle}>кредит</div>
+            <div className={styles.rateTitle}>
+              <a href="#ref" onClick={scrollToReview} onKeyDown={handleKeyDown}>
+                {product.reviewCount}
+                {devOfNum(product.reviewCount, [' отзыв', ' отзыва', ' отзывов'])}
+              </a>
+            </div>
+            <Divider className={styles.hr} />
+            <div className={styles.description}>{product.description}</div>
+            <ProductFeatures characteristics={product.characteristics} />
+            <ProductAdvantages
+              advantages={product.advantages}
+              disadvantages={product.disadvantages}
+            />
+            <Divider className={cn(styles.hr, styles.hr2)} />
+            <div className={styles.actions}>
+              <Button appearance="primary">Узнать подробнее</Button>
+              <Button
+                appearance="ghost"
+                arrow={isReviewOpened ? 'down' : 'right'}
+                className={styles.reviewButton}
+                onClick={() => setIsReviewOpened(!isReviewOpened)}
+              >
+                Читать отзывы
+              </Button>
+            </div>
           </Card>
-        </motion.div>
-      </div>
-    );
-  })
+          <motion.div
+            animate={isReviewOpened ? 'visible' : 'hidden'}
+            variants={variants}
+            initial="hidden"
+            className={cn(styles.reviewWrapper, {
+              [styles.opened]: isReviewOpened,
+              [styles.closed]: !isReviewOpened,
+            })}
+          >
+            <Card color="blue" className={cn(styles.reviews)} ref={reviewRef}>
+              {product.reviews.map(r => (
+                <div key={r._id}>
+                  <Review review={r} />
+                  <Divider />
+                </div>
+              ))}
+              <ReviewForm productId={product._id} isOpened={isReviewOpened} />
+            </Card>
+          </motion.div>
+        </div>
+      );
+    }
+  )
 );
